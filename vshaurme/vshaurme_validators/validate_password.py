33f82ec641728e1758e54d4f086caf8dcbe1a8d9
ikar2021@hotmail.com <ikar2021@hotmail.com>
2019-05-30 21:57:21 +0300

    Remove reduntant if; Remove reduntant parentheses; Fix pep8 warnings

--- vshaurme/vshaurme_validators/validate_password.py
+++ vshaurme/vshaurme_validators/validate_password.py
@@ -1,5 +1,6 @@
 from wtforms import ValidationError
 
+
 def has_digit(password):
     return any(c.isdigit() >= 1 for c in password)
 
@@ -19,15 +20,15 @@ def has_digit_and_upper_lower_exist(password):
 def is_password_short(password):
     min_password_length = 10
     passwd_length = len(password)
-    if passwd_length < min_password_length:
-        return True
-    
+    return passwd_length < min_password_length
+
 
 def is_password_valid(self, field):
     bad_password_message = """Пароль должен:\n
                               быть больше 10 символов,\n
                               содержать буквы и цифры,\n
                               содержать буквы разного регистра.\n"""
-    if (is_password_short(field.data) or 
-        not has_digit_and_upper_lower_exist(field.data)):
+    first_condition = is_password_short(field.data)
+    second_condition = has_digit_and_upper_lower_exist(field.data)
+    if first_condition or not second_condition:
         raise ValidationError(bad_password_message)
